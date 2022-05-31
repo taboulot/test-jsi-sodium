3a16d3580815cee9a96316bbf49244bb1aa16826
hduprat <hugod@bam.tech>
2022-05-31 18:24:15 +0200

    feat: (example) use lib synchronously

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import { compute } from 'fibonachos';
 
 export default function App() {
-  const [result, setResult] = React.useState<string | undefined>();
-
-  React.useEffect(() => {
-    setResult(compute(101));
-  }, []);
-
+  const [rank, setRank] = React.useState(1);
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <View>
+        <Text>Enter Fibonachos rank:</Text>
+        <TextInput
+          style={styles.input}
+          value={rank.toString()}
+          onChangeText={(text) => setRank(Number(text))}
+          keyboardType="number-pad"
+        />
+      </View>
+      <Text style={styles.result}>
+        Result: {rank === 0 ? 'no nachos' : compute(rank - 1)}
+      </Text>
     </View>
   );
 }
@@ -27,5 +33,16 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  input: {
+    padding: 8,
+    backgroundColor: '#FFDDDD',
+    borderColor: '#340000',
+    borderRadius: 8,
+  },
+  result: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 24,
   },
 });
